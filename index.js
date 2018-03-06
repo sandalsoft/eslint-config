33dafaabb41d1637ff731a35551a5804cc69cfdf
Eric Nelson <eric@sandalsoft.com>
2018-03-05 19:18:13 -0600

    INITIAL COMMIT

---
+++ index.js
@@ -0,0 +1,43 @@
+module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      arrowFunctions: true,
+      experimentalObjectRestSpread: true
+    }
+  },
+  env: {
+    es6: true,
+    node: true,
+    jest: true
+  },
+  plugins: ['unicorn', 'no-loops', 'promise', 'security', 'jest'],
+  extends: [
+    'eslint:recommended',
+    'plugin:promise/recommended',
+    'plugin:security/recommended',
+    'plugin:jest/recommended',
+    'plugin:unicorn/recommended'
+  ],
+  rules: {
+    'no-console': 0,
+    quotes: ['error', 'single'],
+    semi: ['warn', 'always'],
+    'no-debugger': 'warn',
+    'no-var': 'error',
+    'no-param-reassign': 'error',
+    'no-undef': 'error',
+    'no-trailing-spaces': 'warn',
+    'eol-last': 0,
+    'no-underscore-dangle': 'warn',
+    'no-alert': 'error',
+    'no-lone-blocks': 'warn',
+    'arrow-parens': 'error',
+    'no-loops/no-loops': 'error',
+    'promise/prefer-await-to-then': 'error',
+    'promise/prefer-await-to-callbacks': 'error'
+  }
+};
---
+++ test-rules.js
@@ -0,0 +1,42 @@
+// Import checker - NOT WORKING, but that's probably ok
+import { log } from '../util';
+
+const boo = require('util');
+console.log(boo);
+
+log('hi');
+
+// Regex optimization - WORKING
+const re = /\w\w*{1,}/;
+console.log(re);
+
+// Immutable - WORKS
+// const c = 'i am immutable!';
+// c = 'jj';
+
+// No loops & security- WORKS
+const books = ['gat', 'hieaf'];
+for (let i = 0; i < books.length; i++) {
+  const book = books[i];
+  console.log(book.title);
+}
+
+// Promises - WORKS
+// Promise.resolve(1)
+//   .then(() => {
+//     throw new Error('oops');
+//   })
+//   .catch(log);
+
+// Throw new error - plugin/unicorn - WORKS
+// const e =  Error('errors are bad mmkay');
+// throw e;
+
+// Not sure what this does but it doen'st work
+// [...'hello'].map(() => 'nope');
+
+// Process.exit() - WORKS
+// process.exit();
+
+// Throw new Error - WORKS
+// throw new Error('hi');
